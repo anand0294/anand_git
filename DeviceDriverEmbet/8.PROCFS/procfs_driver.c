cdaa22ad194550b026f32a3dd13290132ffb4460
anand0294 <anandmohantiwari94@gmail.com>
2018-12-10 23:41:32 +0530

    Procfs entery

--- DeviceDriverEmbet/8.PROCFS/procfs_driver.c
+++ DeviceDriverEmbet/8.PROCFS/procfs_driver.c
@@ -14,6 +14,7 @@
 #include <linux/ioctl.h>
 #include <linux/kdev_t.h>
 #include <linux/device.h>
+#include <linux/proc_fs.h>
 
 #define AMT_WRVALUE _IOW('a', 'a', int32_t * )
 #define AMT_RDVALUE _IOR('a', 'b', int32_t * )
@@ -113,7 +114,7 @@ static ssize_t amt_write( struct file * fp, const char * buff, size_t size, loff
 	return size;
 }
 
-static ssize_t amt_read( struct file * fp, const char * buff, size_t size, loff_t * offset ){
+static ssize_t amt_read( struct file * fp, char __user * buff, size_t size, loff_t * offset ){
 	printk(KERN_INFO "Read operation invoked...\n");
 	return size;
 }
@@ -122,11 +123,11 @@ static ssize_t amt_read( struct file * fp, const char * buff, size_t size, loff_
 unsigned long amt_ioctl ( struct file * fp, unsigned int cmd, unsigned long arg ){
 	printk(KERN_INFO "IOCTL operation Invoked\n");
 	switch(cmd){
-		case AMT_RDVALUR:
+		case AMT_RDVALUE:
 			copy_to_user( (int32_t *)arg, &value, sizeof(value) );
 			break;
 		case AMT_WRVALUE:
-			copy_form_user( &value, (int32_t *)arg, sizeof(value) );
+			copy_from_user( &value, (int32_t *)arg, sizeof(value) );
 			printk(KERN_INFO "Value written to kernel is : %d\n", value);
 			break;
 		default:
@@ -137,16 +138,65 @@ unsigned long amt_ioctl ( struct file * fp, unsigned int cmd, unsigned long arg 
 }
 
 	/**Initialization Routine **/
-static int __init amt_init(){
-	if(){
-		printk(KERN_INFO "Failed to allocate");
+static int __init amt_init( void ){
+	/*Allocation of Major and Minor Number*/
+	if( ( alloc_chrdev_region (&amt_dev, firstminor, noofdevice, "AMT_NODENO") ) < 0 ){
+		printk(KERN_INFO "Failed to allocate Major and Minor no\n");
 		return -1;
 	}
+	printk(KERN_INFO "Major No: %d, Minor No: %d\n", MAJOR(amt_dev), MINOR(amt_dev) );
+	
+	/* Adding cdev structure with file_operations*/
+
+	cdev_init( &amt_cdev, &amt_fops );
+	
+	/*Addition of Device to the system*/
+	if( cdev_add( &amt_cdev, amt_dev, noofdevice ) < 0){
+		printk(KERN_INFO "Failed to add device to the system\n");
+		goto r_class;
+	}
+
+	/*Creating class structure*/
+	amt_class = class_create( THIS_MODULE, "AMT_CLASS");
+	if( amt_class == NULL ){
+		printk(KERN_INFO "Failed to create Class\n");
+		goto r_class;
+	}
+	
+	/*Creating Device*/
+	if( device_create (amt_class, NULL, amt_dev, NULL, "MOHAN_CHAR_DRIVER") == NULL ){
+		printk(KERN_INFO "Failed to create device in the system\n");
+		goto r_device;
+	}
+
+	/*Creating proc entry*/
+	proc_create ("amt_proc", 0666,NULL, &proc_fops );
+	printk(KERN_INFO "Kernel Inserted Successfully\n");
 
 	return 0;
 r_device:
 	class_destroy(amt_class);
 r_class:
-	unregister(amt_dev, noofdevice);
-
+	unregister_chrdev_region ( amt_dev, noofdevice );
+	return -1;
 }
+
+
+static void __exit amt_exit ( void ){
+	remove_proc_entry("amt_proc", NULL );
+	device_destroy( amt_class, noofdevice );
+	class_destroy( amt_class );
+	cdev_del( &amt_cdev );
+	unregister_chrdev_region( amt_dev, noofdevice );
+	printk(KERN_INFO "Kernel module removed successfully...\n");
+	return;
+}
+
+
+module_init ( amt_init );
+module_exit ( amt_exit );
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Shows to create proc entry");
+MODULE_AUTHOR("ANAND MOHAN TIWARI");
+MODULE_VERSION ("0.0.9");
