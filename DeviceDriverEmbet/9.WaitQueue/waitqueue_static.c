9c41cf02697936e106fb5fc3b4e0386316e7de40
anand0294 <anandmohantiwari94@gmail.com>
2019-01-16 14:46:39 +0530

    Wait Queue Static Method

---
+++ DeviceDriverEmbet/9.WaitQueue/waitqueue_static.c
@@ -0,0 +1,10 @@
+/*
+ *AUTHOR: ANAND MOHAN TIWARI
+ *Purpose: This show the use of Static waitqueue method. When a particular
+ * event will occur then it will execute a particular task.
+ */
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/device.h>
