19084b09765f3abfefc0e3b8e0d755fc9e205c39
Anand Mohan Tiwari <39147382+anand0294@users.noreply.github.com>
2019-01-30 12:14:28 +0530

    Add files via upload

---
+++ linux_system_programming/socket/connection_client.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <netdb.h>
+#include <netinet/in.h>
+
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+   int sockfd, portno, n;
+   struct sockaddr_in serv_addr;
+   struct hostent *server;
+   
+   char buffer[256];
+//mohan declaration starts
+        char buf[20];
+	unsigned int crc, len;
+  //mohan declaration ends 
+   
+   if (argc < 3) {
+      fprintf(stderr,"usage %s hostname port\n", argv[0]);
+      exit(0);
+   }
+	
+   portno = atoi(argv[2]);
+   
+   /* Create a socket point */
+   sockfd = socket(AF_INET, SOCK_STREAM, 0);
+   
+   if (sockfd < 0) {
+      perror("ERROR opening socket");
+      exit(1);
+   }
+	
+   server = gethostbyname(argv[1]);
+   
+   if (server == NULL) {
+      fprintf(stderr,"ERROR, no such host\n");
+      exit(0);
+   }
+   
+   bzero((char *) &serv_addr, sizeof(serv_addr));
+   serv_addr.sin_family = AF_INET;
+   bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+   serv_addr.sin_port = htons(portno);
+  	 
+   /* Now connect to the server */
+   if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+      perror("ERROR connecting");
+      exit(1);
+   }
+   
+   /* Now ask for a message from the user, this message
+      * will be read by server
+   */
+	
+   printf("Please enter the message: \n");
+//mohan starts
+	printf("Enter '#update' to update or 'proceed' to cotinue\n");
+	bzero(buf, 20);
+	fgets(buf, 20, stdin );
+	write(sockfd,buf,20 );
+	
+	bzero(buf, 20);
+	read(sockfd, buf, 20);
+	printf("%s\n", buf);
+	scanf("Enter CRC %u", &crc );	
+	write(sockfd,&crc, sizeof(crc) );
+	read(sockfd, buf, 20);
+	printf("%s\n", buf);
+	scanf("Enter length of file : %u", &len);
+	write( sockfd, &len, sizeof(len) );
+	read(sockfd, buf, 20);
+	printf("%s\n", buf);
+	write(sockfd, "Pass the Buffer which is read from", len);
+//mohan ends
+   bzero(buffer,256);
+   fgets(buffer,255,stdin);
+   
+   /* Send message to the server */
+   n = write(sockfd, buffer, strlen(buffer));
+   
+   if (n < 0) {
+      perror("ERROR writing to socket");
+      exit(1);
+   }
+   
+   /* Now read server response */
+   bzero(buffer,256);
+   n = read(sockfd, buffer, 255);
+   
+   if (n < 0) {
+      perror("ERROR reading from socket");
+      exit(1);
+   }
+	
+   printf("%s\n",buffer);
+   return 0;
+}
